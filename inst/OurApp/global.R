53272cd5bcf7a12719e9005fc795c5276b012bd9
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-10-01 13:41:48 +0100

    Reformat and fix line break in doc

--- inst/OurApp/global.R
+++ inst/OurApp/global.R
@@ -1,2 +1,3 @@
+library(a2bcovid)
 library(shiny)
 library(ggplot2)
--- inst/OurApp/server.R
+++ inst/OurApp/server.R
@@ -1,12 +1,10 @@
 
-exampledir <- "/Users/chris/work/covid/cill/whoinfected/tests/local"
+exampledir <- "/Users/chris/work/covid/cill/a2bcovid/tests/local"
 exampledat <- list(pat="cluster_D_genetic_temporal_data_NoPII_20200807.csv",
                    mov="hcw_movement_20200811_D_NoPII.csv",
                    ward="ward_movement_network_edit_anonymised_20200811_NoPII.csv",
                    ali="Seqs_editN20_manali_plus.fa")
 exampledat <- lapply(exampledat, function(x)file.path(exampledir, x))
-
-
 
 shinyServer(function(input, output, session) {
 
@@ -27,6 +25,6 @@ shinyServer(function(input, output, session) {
             theme(axis.text.x = element_text(angle = 90, vjust=0.5),
                   legend.title = element_blank())
     })
-    
+
 })
 
